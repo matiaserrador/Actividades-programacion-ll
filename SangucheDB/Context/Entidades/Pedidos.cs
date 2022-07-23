8a3627d1218d40e056f9ebb6d9efae34f2490cad
Matias Errador <loco.matu@hotmail.com>
2022-07-22 23:19:05 -0300

    Agregar archivos de proyecto.

---
+++ SangucheDB/Context/Entidades/Pedidos.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SangucheDB.Context.Entidades
+{
+    ///Esto define tu Index, la Primery Key
+    [Index(nameof(CodigoPedido), Name = "Pedido_Codpedido", IsUnique = true)]
+    public class Pedidos
+    {
+        ///Esto viene de la DataAnnotation que esto NO permite dejar valores nulos, por eso aplico los Required.
+        [Required(ErrorMessage = "La dirección es obligatoria")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El nombre del cliente es obligatorio.")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public string NombreCliente { get; set; }
+
+        [Required(ErrorMessage = "La fecha del pedido es obligatorio.")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public string Fecha { get; set; }
+
+        [Required(ErrorMessage = "El horario del pedido es obligatorio.")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public string Horario { get; set; }
+
+        [Required(ErrorMessage = "El monto del pedido es obligatorio.")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public int Monto { get; set; }
+
+        [Required(ErrorMessage = "El tipo de sanguche del pedido es obligatorio.")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public string TipoSandwich { get; set; }
+
+        [Required(ErrorMessage = "El codigo del pedido es obligatorio.")]
+        [MaxLength(150, ErrorMessage = "El campo tiene como máximo {1} caracteres.")]
+        public int CodigoPedido { get; set; }
+
+    }
+}
---
+++ SangucheDB/Context/dbcontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SangucheDB.Context.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SangucheDB.Context
+{
+    public class dbcontext : DbContext
+    {
+        public DbSet<Pedidos> pedidos { get; set; }
+        public dbcontext(DbContextOptions<dbcontext> options) : base(options) 
+        {
+            
+        }
+       
+    }
+}
